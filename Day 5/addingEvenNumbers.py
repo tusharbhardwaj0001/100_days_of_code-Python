41919c734ea2e227124adcf055ebc54867c389ad
Tushar Bhardwaj <tusharbhardwaj.313@gmail.com>
2022-02-05 22:35:01 +0530

    Create addingEvenNumbers.py
    You are going to write a program that calculates the sum of all the even numbers from 1 to 100. Thus, the first even number would be 2 and the last one is 100:

---
+++ Day 5/addingEvenNumbers.py
@@ -0,0 +1,10 @@
+#Method 1:
+#Write your code below this row 👇
+Sum = 0
+for i in range(2,101,2):
+    Sum += i
+print(Sum)
+
+
+#We can use direct formula that is :
+# sum of first n even numbers = n*(n+1)
