8734c2057418b0ac30992d2e411be5191f22056f
Tushar Bhardwaj <tusharbhardwaj.313@gmail.com>
2022-02-09 01:42:02 +0530

    Create hurdle2.py

---
+++ Day 6/hurdle2.py
@@ -0,0 +1,25 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def turn_special():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif wall_in_front():
+        turn_special()
+
+
+
+
+
