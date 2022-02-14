e7a0a252bce0f3681e511a8e88a3743388c0ae3d
Tushar Bhardwaj <tusharbhardwaj.313@gmail.com>
2022-02-14 23:55:06 +0530

    Update and rename Exercise 2 - Debugging Practice to Exercise 2 - Debugging Practice.py

--- Day 1/Exercise 2 - Debugging Practice.py
+++ Day 1/Exercise 2 - Debugging Practice.py
@@ -1,3 +1,4 @@
+"""
 Instruction : Look at the code in the code editor on the right. There are errors in all of the lines of code. Fix the code so that it runs without errors.
 
 Given code : #Fix the code below 👇
@@ -8,7 +9,7 @@ print("String Concatenation is done with the "+" sign.")
 print(("New lines can be created with a backslash and n.")
 
 Correct Code: 
-
+"""
 print("Day 1 - String Manipulation")
 print("String Concatenation is done with the"+ '"+"'+" sign.")
 print('e.g. print("Hello " + "world")')
