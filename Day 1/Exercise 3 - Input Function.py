32a5699d3c17ad46b94f9906dc0624d43c806a09
Tushar Bhardwaj <tusharbhardwaj.313@gmail.com>
2022-02-14 23:55:47 +0530

    Update and rename Exercise 3 - Input Function to Exercise 3 - Input Function.py

--- Day 1/Exercise 3 - Input Function.py
+++ Day 1/Exercise 3 - Input Function.py
@@ -1,9 +1,11 @@
+"""
 Problem : Write a program that prints the number of characters in a user's name.
 
 Example Input : Tushar 
 Example output : 6
 
 Code : 
+"""
 #Write your code below this line 👇
 
 name  = input("What is your name? ")
